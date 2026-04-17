15cf83b26a146b4b5d8a28325ebb52d7db6d9896
leo-bozhinoski <leo.bozinoski@gmail.com>
2026-04-17 11:40:43 +0200

    Added a new gesture for nerd pointing up

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from mediapipe.tasks.python import BaseOptions
 # ---------------- HAND SKELETON NODES ---------------- #
 
 HAND_CONNECTIONS = [
-    (0, 1), (1, 2), (2, 3), (3, 4),
-    (0, 5), (5, 6), (6, 7), (7, 8),
-    (5, 9), (9, 10), (10, 11), (11, 12),
-    (9, 13), (13, 14), (14, 15), (15, 16),
-    (13, 17), (17, 18), (18, 19), (19, 20),
+    (0, 1), (1, 2), (2, 3), (3, 4), # thumb
+    (0, 5), (5, 6), (6, 7), (7, 8), # index
+    (5, 9), (9, 10), (10, 11), (11, 12), # middle
+    (9, 13), (13, 14), (14, 15), (15, 16), # ring
+    (13, 17), (17, 18), (18, 19), (19, 20), # pinky
     (0, 17)
 ]
 
@@ -88,14 +88,25 @@ def is_thumbs_up(points, handedness):
 
     return all_tucked and all_above_eachother and thumb_extended and thumb_upright
 
+# ---------------- GESTURE: POINT UP LIKE A NERD ---------------- #
+
+def is_pointing_up(points):
+    index_up = points[8][1] < points[6][1]
+
+    middle_down = points[12][1] > points[10][1]
+    ring_down   = points[16][1] > points[14][1]
+    pinky_down  = points[20][1] > points[18][1]
+
+    return index_up and middle_down and ring_down and pinky_down
+
 # ---------------- show image if gesture detected ---------------- #
 
 def show_overlay(show, image):
     if show:
-        cv2.imshow("Thumbs Up", image)
+        cv2.imshow("Linganguli", image)
     else:
         try:
-            cv2.destroyWindow("Thumbs Up")
+            cv2.destroyWindow("Linganguli")
         except:
             pass
 
@@ -113,10 +124,12 @@ detector = vision.HandLandmarker.create_from_options(options)
 cap = cv2.VideoCapture(0)
 
 thumbsUp_image = cv2.imread("assets/thumbs_up.png")
+pointingUp_image = cv2.imread("assets/point_up.png")
 
 # ----------- anti-flickering variables ----------- #
 
-gestureFrames = 0
+thumbs_frames = 0
+pointing_frames = 0
 showImage = False
 
 # ---------------- MAIN DETECTION LOOP ---------------- #
@@ -132,6 +145,7 @@ while True:
     result = detector.detect(mp_image)
 
     thumbs_up_detected = False
+    pointing_up_detected = False
 
     if result.hand_landmarks:
         for i, hand in enumerate(result.hand_landmarks):
@@ -145,18 +159,33 @@ while True:
             if is_thumbs_up(points, handedness):
                 thumbs_up_detected = True
 
+            if is_pointing_up(points):
+                pointing_up_detected = True
+
     # -------- no flickering -------- #
 
     if thumbs_up_detected:
-        gestureFrames += 1
+        thumbs_frames += 1
     else:
-        gestureFrames -= 1
+        thumbs_frames -= 1
 
-    gestureFrames = max(0, min(gestureFrames, 10))
-    showImage = gestureFrames > 5
+    thumbs_frames = max(0, min(thumbs_frames, 10))
+    showThumbs = thumbs_frames > 3
 
+    if pointing_up_detected:
+        pointing_frames += 1
+    else:
+        pointing_frames -= 1
 
-    show_overlay(showImage, thumbsUp_image)
+    pointing_frames = max(0, min(pointing_frames, 10))
+    showPointing = pointing_frames > 3
+
+    if showPointing:
+        show_overlay(True, pointingUp_image)
+    elif showThumbs:
+        show_overlay(True, thumbsUp_image)
+    else:
+        show_overlay(False, None)
 
     cv2.imshow("ESC to exit", frame)
 
